4b712b421741b60fb8f01f70290a6e2c441b4438
luis110487 <42459937+luis110487@users.noreply.github.com>
2018-08-24 17:35:52 -0500

    cambio

---
+++ Sake/Sake.Domain/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sake.Domain.Models
+{
+  public class DataContext: DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+        }
+    }
+}
---
+++ Sake/Sake.backend/Models/LocalDataContext.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Sake.backend.Models
+
+{
+    using Domain.Models;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sake.Common.Models.Product> Products { get; set; }
+    }
+}
